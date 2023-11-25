7eb92a056924430d8d929deefbcb4ee56f0fc25e
x1s <x1s@L-PC69RV9VT6.local>
2023-11-25 05:12:11 +0000

    adding E2E tests

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ steps to run application locally
 
 # BACKEND
 
+API Documentation (swagger) available at http://localhost:<PORT>/docs if you are running locally
+
 - create a mongo instance locally or using https://www.mongodb.com/ (cloud)
 - create .env file and add mongodb url as following
 - add a PORT to your .env file
 
-## ENV SERVER SAMPLE 
+### ENV SERVER SAMPLE 
 
 `MONGODB_URL=http://localhost:5555`
 
@@ -16,6 +18,10 @@ steps to run application locally
 
 - run command `npm install` to start server
 - run command `npm run dev` to start server
+
+### BACKEND E2E TESTS
+
+- run command `npm run test` to start server
 
 
 -----------
@@ -26,7 +32,7 @@ steps to run application locally
 - take the server url that you are running your server.  e.g. : http://localhost:3333
 - create .env file and add VITE_API url as following
 
-# ENV CLIENT SAMPLE 
+### ENV CLIENT SAMPLE 
 
 `VITE_API=http://localhost:3333`
 
---
+++ server/src/__tests__/E2E/task.test.ts
@@ -0,0 +1,68 @@
+import supertest from "supertest";
+import express, { Express } from "express";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import routes from "../../routes";
+
+const app = express();
+app.use(express.json());
+
+routes(app);
+
+const request = supertest(app);
+
+beforeAll(async () => {
+  const mongoServer = await MongoMemoryServer.create();
+
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoose.connection.close();
+});
+
+describe("Task API", () => {
+  let taskId: string;
+
+  test("Create Task", async () => {
+    const response = await request.post("/task").send({ title: "Test Task" });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("_id");
+    taskId = response.body._id;
+  });
+
+  test("Get All Tasks", async () => {
+    const response = await request.get("/tasks");
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+
+  test("Update Task", async () => {
+    const response = await request
+      .put(`/task/update/${taskId}`)
+      .send({ title: "Updated Task" });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("_id", taskId);
+    expect(response.body.title).toBe("Updated Task");
+  });
+
+  test("Update Non existent valid Task", async () => {
+    const response = await request
+      .put(`/task/update/656174712480df0402dee33c`)
+      .send({ title: "Updated Task" });
+    expect(response.status).toBe(404);
+  });
+
+  test("Delete Task", async () => {
+    const response = await request.delete(`/task/delete/${taskId}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("_id", taskId);
+  });
+
+  test("Delete Non existent valid Task", async () => {
+    const response = await request.delete(`/task/delete/656174712480df0402dee33c`);
+    expect(response.status).toBe(404);
+  });
+});
